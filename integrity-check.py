221df41d9e4af3d120075fa1fba9e988fa3f5464
Jonathan <villagomezhjonathan@gmail.com>
2026-01-04 18:52:25 -0500

    created db connecion

--- integrity-check.py
+++ integrity-check.py
@@ -5,15 +5,26 @@
 import os
 import sys
 import subprocess
+import sqlite3
+
+connection = sqlite3.connect("fim.db")
+db = connection.cursor()
+
+try: 
+  db.execute("SELECT * FROM files")
+except:
+  db.execute("CREATE TABLE files(file_name, md5, file_path)")
+
 
 def check(path):
   files = os.listdir(path)
   
   for f in files:
     hash = subprocess.run(["md5sum", f], capture_output=True).stdout.splitlines()
-    print(hash)
 
 if __name__ == '__main__':
   path_arg = sys.argv[1]
   
   check(path_arg)
+
+  db.close()
