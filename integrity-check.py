257550b1d21fb1f66b8855c85ead93aed23e0b76
Jonathan <villagomezhjonathan@gmail.com>
2026-01-04 19:24:20 -0500

    implemented db

--- integrity-check.py
+++ integrity-check.py
@@ -10,23 +10,30 @@ import sqlite3
 connection = sqlite3.connect("fim.db")
 db = connection.cursor()
 
-try: 
-  db.execute("SELECT * FROM files")
-except:
-  db.execute("CREATE TABLE files(file_name, md5, file_path)")
-
+db.execute("CREATE TABLE IF NOT EXISTS files(file_name, md5, file_path)")
 
 def check(path):
+  if not os.path.isdir(path):
+    print(f"Error: Directory '{path}' not found.")
+    return
+  
   files = os.listdir(path)
+
   
   for f in files:
-    hash = subprocess.run(["md5sum", f], capture_output=True).stdout.splitlines()
-    db.execute(f"SELECT md5 FROM files WHERE md5={hash}")
-    db.fetchone()
+    output = subprocess.run(["md5sum", f], capture_output=True, text=True).stdout
+    hash = output[:32]
+
+    res = db.execute("SELECT * FROM files WHERE md5=?", (hash,))
+
+    if res.fetchone() is None:
+      print(f"New file found: {f}")
+      db.execute("INSERT INTO files VALUES (?, ?, ?)", (f, hash, path))
+      connection.commit() 
 
 if __name__ == '__main__':
   path_arg = sys.argv[1]
   
   check(path_arg)
 
-  db.close()
+  connection.close()
