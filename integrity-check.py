0a350b6af26e7383614a41617ecdc8b5887719a5
Jonathan <villagomezhjonathan@gmail.com>
2026-01-07 00:04:16 -0500

    added conditionl for setting path

--- integrity-check.py
+++ integrity-check.py
@@ -35,10 +35,12 @@ if __name__ == '__main__':
   path_arg = None
 
   if len(sys.argv) < 2:
+    pwd = subprocess.run(["pwd"], capture_output=True, text=True).stdout
     path_arg = "."
   else:
     path_arg = sys.argv[1]
   
-  check(path_arg)
+  print(path_arg)
+  # check(path_arg)
 
   connection.close()
